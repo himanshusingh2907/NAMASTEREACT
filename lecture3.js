2b497817362439cd842ca47e3a4423ec5c074fb1
himanshusingh2907 <130087437+himanshusingh2907@users.noreply.github.com>
2024-09-23 00:27:10 +0530

    Add files via upload

---
+++ lecture3.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading1=React.createElement("h1",{id:"head1"},"first heading");
+const heading2=React.createElement("h1",{id :"head2"},"second heading");
+const div1=React.createElement("div",{id:"container"},[heading1,heading2]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(div1);
